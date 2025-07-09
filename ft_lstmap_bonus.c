61371912ef17460d87d79c2f7dbdfb1bd77a4f59
Nikita Luchinin <nluchini@1-B-12.42heilbronn.de>
2025-07-09 14:55:25 +0200

    ft_lstmap: resolve memory leak bug

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,73 +6,85 @@
 /*   By: nluchini <nluchini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 15:01:12 by nluchini          #+#    #+#             */
-/*   Updated: 2025/07/08 11:59:00 by nluchini         ###   ########.fr       */
+/*   Updated: 2025/07/09 14:51:20 by nluchini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+static void	*ft_free(t_list **list, void *content, void (*del)(void *))
 {
-	t_list	**new_list;
-	t_list	*new_node;
-
-	if (!lst || !f)
-		return (NULL);
-	new_list = (t_list **)ft_calloc(1, sizeof(t_list *));
-	if (!new_list)
-		return (NULL);
-	*new_list = NULL;
-	while (lst)
-	{
-		new_node = ft_lstnew(f(lst->content));
-		if (!new_node)
-			return (ft_lstclear(new_list, del), NULL);
-		ft_lstadd_back(new_list, new_node);
-		lst = lst->next;
-	}
-	new_node = *new_list;
-	free(new_list);
-	return (new_node);
+	if (content)
+		del(content);
+	ft_lstclear(list, del);
+	return (NULL);
 }
 
-/*
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new_list;
+	t_list	*new_node;
+	void	*mapped;
+
+	if (!lst || !f || !del)
+		return (NULL);
+	new_list = NULL;
+	while (lst)
+	{
+		mapped = f(lst->content);
+		new_node = ft_lstnew(mapped);
+		if (!new_node)
+			return (ft_free(&new_list, mapped, del));
+		ft_lstadd_back(&new_list, new_node);
+		lst = lst->next;
+	}
+	return (new_list);
+}
+
+/* 
 #include <stdio.h>
 #include <stdlib.h>
+
+void leaks(void)
+{
+	system("leaks a.out");
+}
+
 void	ft_print_result(t_list *elem)
 {
 	while (elem)
 	{
-		printf("%d ", *(int *)(elem->content));
+		printf("%s\n", (char *)(elem->content));
 		elem = elem->next;
 	}
 }
 
 void *ft_map(void *content)
 {
-	int *value = (int *)content;
-	*value *= 2; // Example operation: double the value
-	return value;
+	char *value = (char *)content;
+	char *new = (char *)malloc(30);
+	
+	sprintf(new, "__%s", value);
+	return (new);
 }
 
 
 int main()
-{
+{	
+	// to moc malloc i use global variable
 	t_list **array;
 	array = (t_list **)malloc(sizeof(t_list *));
 	*array = NULL;
-	int a = 42;
-	int b = 84;
-	int c = 126;
 
-	ft_lstadd_back(array, ft_lstnew(&a));
-	ft_lstadd_back(array, ft_lstnew(&b));
-	ft_lstadd_back(array, ft_lstnew(&c));
+	ft_lstadd_back(array, ft_lstnew("Hello"));
+	ft_lstadd_back(array, ft_lstnew("World"));
+	ft_lstadd_back(array, ft_lstnew("42"));
 
 	ft_print_result(*array);
 	printf("\n");
-
+	ft_lstclear(array, free);
+	atexit(leaks);
 	t_list *new_list = ft_lstmap(*array, ft_map, free);
 	ft_print_result(new_list);
 } */
