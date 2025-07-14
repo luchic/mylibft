4da35549c3672b5f653f6c003fef5a876ef2a327
Nikita Luchinin <nluchini@1-B-12.42heilbronn.de>
2025-07-14 11:49:34 +0200

    ft_putlnbr_fd.c: Use ft_putulnbr_base_fd function inside

--- ft_putlnbr_fd.c
+++ ft_putlnbr_fd.c
@@ -6,51 +6,21 @@
 /*   By: nluchini <nluchini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/14 09:40:05 by nluchini          #+#    #+#             */
-/*   Updated: 2025/07/14 11:42:00 by nluchini         ###   ########.fr       */
+/*   Updated: 2025/07/14 11:49:02 by nluchini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <limits.h>
 #include "libft.h"
-
-static int	putnbr(unsigned long nb, int fd)
-{
-	char	out;
-	int		res;
-
-	if (nb / 10 == 0)
-	{
-		out = nb + '0';
-		return (write(fd, &out, 1));
-	}
-	out = nb % 10 + '0';
-	nb = nb / 10;
-	res = putnbr(nb, fd);
-	if (res == -1)
-		return (-1);
-	if (write(fd, &out, 1) == -1)
-		return (-1);
-	return (res + 1);
-}
 
 int	ft_putlnbr_fd(long n, int fd)
 {
 	int				res;
 	unsigned long	nbr;
+	char			*base;
 
 	nbr = n;
-	if (n < 0)
-	{
-		if (write(fd, "-", 1) == -1)
-			return (-1);
-		res = putnbr(nbr, fd);
-		if (res == -1)
-			return (-1);
-		return (res + 1);
-	}
-	else
-		return (putnbr(nbr, fd));
+	base = "0123456789";
+	ft_putulnbr_base_fd(nbr, base, fd);
 }
 
 // #include <stdio.h>
